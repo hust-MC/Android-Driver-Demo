89fd7194893602fccb8f1a3f7f2092da9a3d3cb7
MC <251887584@qq.com>
2014-07-30 19:04:43 +0800

    debug spinLock delay

--- Concurrency/spin_lock/src/spin_lock.c
+++ Concurrency/spin_lock/src/spin_lock.c
@@ -7,54 +7,75 @@
 
 #include <asm/uaccess.h>
 #include <asm/spinlock.h>
+//#include <stdio.h>
+//#include <stdlib.h>
 
-#include <stdlib.h>
+#define DEVICE_NAME "spin_lock"
 
-static *data = "read";
+static char *data = "read\n";
 static DEFINE_SPINLOCK(lock);
+static char read_flag = 0;
 
 static ssize_t spinLock_read(struct file *file, char __user *buf, size_t count,
 		loff_t *ppos)
 {
-	int size = strlen(data);
-	if (copy_to_user(buf, (void*) data, size))
+	if (!read_flag)
 	{
-		return -EINVAL;
-	}
-	else
-	{
-		if (spin_trylock(&lock))
+
+		int size = strlen(data);
+		if (copy_to_user(buf, (void*) data, size))
 		{
-			//在此做个延时，用于模拟临界区代码执行
-			mdelay(10000);
-			spin_unlock(&lock);
+			return -EINVAL;
 		}
 		else
 		{
-			return -EBUSY;
+			if (spin_trylock(&lock))
+			{
+				//在此做个延时，用于模拟临界区代码执行
+//				mdelay(10000);
+				//test
+				volatile long long a = 0;
+				int i, j;
+				for (i = 0; i < 100000; i++)
+					for (j = 0; j < 100000; j++)
+						a++;
+				spin_unlock(&lock);
+			}
+			else
+			{
+				return -EBUSY;
+			}
+			read_flag = 1;
+			return size;
 		}
-		return size;
+	}
+	else
+	{
+		read_flag = 0;
+		printk("read_flag = 0\n");
+		return 0;
 	}
 }
 
 static ssize_t spinLock_write(struct file *file, const char __user *buf,
 		size_t count, loff_t *ppos)
 {
-	char *cmd = (char *) malloc(sizeof(char *));
+//	char *cmd = (char *) malloc(sizeof(char *));
+	char cmd[10] = { 0 };
 	if (copy_from_user(cmd, (void*) buf, count))
 	{
 		return -EINVAL;
 	}
 	else
 	{
-		if(strcmp("lock\n",cmd) == 0)
+		if (strcmp("lock\n", cmd) == 0)
 		{
 			spin_lock(&lock);
 			spin_unlock(&lock);
 		}
-		else if(strcmp("trylock\n",cmd) == 0)
+		else if (strcmp("trylock\n", cmd) == 0)
 		{
-			if(spin_trylock(&lock))
+			if (spin_trylock(&lock))
 			{
 				printk("spin_lock is available\n");
 				spin_unlock(&lock);
@@ -69,10 +90,29 @@ static ssize_t spinLock_write(struct file *file, const char __user *buf,
 	}
 }
 
-static struct file_operations fops=
+static struct file_operations dev_fops = { .owner = THIS_MODULE, .read =
+		spinLock_read, .write = spinLock_write };
+
+static struct miscdevice misc = { .minor = MISC_DYNAMIC_MINOR, .name =
+DEVICE_NAME, .fops = &dev_fops };
+
+static int spinLock_init(void)
 {
-		.owner = THIS_MODULE,
-		.read = spinLock_read,
-		.write = spinLock_write
-};
+	printk("spinLock_init success\n");
+
+	int ret;
+	ret = misc_register(&misc);
+	return ret;
+}
+
+static void spinLock_exit(void)
+{
+	printk("spinLock_exit success\n");
+	misc_deregister(&misc);
+}
+
+module_init(spinLock_init);
+module_exit(spinLock_exit);
+
+MODULE_LICENSE("GPL");
 
