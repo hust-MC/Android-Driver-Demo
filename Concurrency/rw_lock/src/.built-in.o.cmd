23e449ce2ee98c239bf45a574e1c9cd2a4d7ecc0
MC <251887584@qq.com>
2014-08-18 20:22:32 +0800

    build

--- Concurrency/rw_lock/src/.built-in.o.cmd
+++ Concurrency/rw_lock/src/.built-in.o.cmd
@@ -1,1 +1,1 @@
-cmd_/root/android/demo/Concurrency/rw_lock/src/built-in.o :=  rm -f /root/android/demo/Concurrency/rw_lock/src/built-in.o; ar rcsD /root/android/demo/Concurrency/rw_lock/src/built-in.o
+cmd_/root/android/demo/Concurrency/rw_lock/src/built-in.o :=  rm -f /root/android/demo/Concurrency/rw_lock/src/built-in.o; arm-linux-ar rcsD /root/android/demo/Concurrency/rw_lock/src/built-in.o
